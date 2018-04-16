3fdb1f873d66115ae29d40ab44bf1f465993c5d4
informatika 1 <ik1wecmdn@gmail.com>
2018-04-16 09:54:10 +0700

    Pertemuan 1

---
+++ src/pages/detail-profil/detail-profil.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { DetailProfilPage } from './detail-profil';
+
+@NgModule({
+  declarations: [
+    DetailProfilPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(DetailProfilPage),
+  ],
+})
+export class DetailProfilPageModule {}
